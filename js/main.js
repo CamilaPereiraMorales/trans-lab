d4bf7bb6b02479ce9b5cec22e5b4a607f5ccfa71
Camila <camipereira2111@gmail.com>
2018-06-28 12:10:42 -0400

    avances

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-/* Holiii acá va tu código también */
+//con esto tomo el numero de codigo que ingrese el usuario
+let input = document.querySelector('input');
+function getBip(num){
+
+        // 13 is enter
+          // code for enter
+          input.addEventListener("keypress",(enter)=>{
+            let key = enter.which || enter.keyCode;
+            if (key === 13) {
+            
+
+
+          fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${num}`) 
+            .then(response => response.json())
+            .then(data => {
+                let numeroTarjeta =[`N&ordm; tarjeta bip! `];
+                let saldoTarjeta = [`Saldo  tarjeta`];
+                let fechaSaldo = [`Fecha saldo`];
+                
+                console.log(data);
+                
+             })
+            }
+    
+        })
+    }
+     console.log(getBip('77108068'))        
+//{"N&ordm; tarjeta bip! ": "77108068", Estado de contrato: "Contrato Activo", Saldo  tarjeta: "$220", Fecha saldo: "13/06/2018 11:09"}
+
+
+    
+     
+     
+         
+ 
+        
+           
+      
+    
+    
